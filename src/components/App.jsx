9ff14439ec619ec7f6304910f183819c26ca89ae
katy-holder <gold19920929@gmail.com>
2022-06-28 22:11:36 +0200

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Profile } from "./Profile/Profile";
 import user from "./Profile/user.json";
 import { Statistics } from "./Statistics/Statistics";
+import data from "./Statistics/data.json";
 
 
 export const App = () => {
@@ -12,6 +13,6 @@ export const App = () => {
     avatar={user.avatar}
     stats={user.stats}
     />
-    <Statistics/>
+    <Statistics title="Upload stats" stats={data} />
   </div>
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,9 @@
+import data from "../Statistics/data.json";
+
 export const Statistics = () => {
-    return
+    return <section className="statistics">
+  <h2 className="title">Upload stats</h2>
+    <ul className="stat-list">{data.map(el => <li className="item" key={el.id}><span className="label">{el.label}</span><span className="percentage">{el.percentage}%</span></li>)}
+  </ul>
+</section>
 }
